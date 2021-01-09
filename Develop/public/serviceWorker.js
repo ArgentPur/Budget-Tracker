fc25c4116c0aca21cabfab547d643efd54ca6359
Zachary Lynn <zalynn25@hotmail.com>
2021-01-08 20:46:29 -0500

    serviceWorker

---
+++ Develop/public/serviceWorker.js
@@ -0,0 +1,73 @@
+const cacheFiles = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/index.js",
+    "icons/icon-192x192.png",
+    "icons/icon-512x512.png",
+    "/manifest.json",
+    
+  ];
+  
+  const staticName = "static-cache-v2";
+  const dataName = "data-cache-v1";
+  
+  // install
+  self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(staticName).then(cache => {
+        console.log("Your files have been cached successfully.");
+        return cache.addAll(cacheFiles);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== staticName && key !== dataName) {
+              console.log("Cached data deleted.", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  
+  self.addEventListener("fetch", function(evt) {
+    // cache requests to "/api/"
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(dataName).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // Clone valid responses
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    };
+
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+      })
+    );
+  });
